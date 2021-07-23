def426d8d13f24ebfed157faf1ab940b463727cc
automationtrainingcenter <atcsurya@gmail.com>
2021-07-23 12:44:16 +0530

    constructor overloading and chaining, encapsulation

--- src/methods_and_constructors/ConstructorDemoTwo.java
+++ src/methods_and_constructors/ConstructorDemoTwo.java
@@ -7,29 +7,41 @@ public class ConstructorDemoTwo {
     String course;
     long phNum;
     double fee;
+    String email;
+    int pincode;
 
 
     // default constructor
     public ConstructorDemoTwo(){
-        id = 100;
-        name = "anusha";
-        course ="selenium";
-        phNum = 9876543210L;
-        fee = 12000.00;
+        this(100, "anusha", "selenium", 9876543210L, 12000.00, "", 500050);
     }
 
     // parameterized constructor
     public ConstructorDemoTwo(int id, String name, String course, long phNum, double fee) {
+        this(id, name, course, phNum, fee, "", 500050);
+    }
+
+    public ConstructorDemoTwo(int id, String name, String course, long phNum, double fee, String email) {
+        this(id, name, course, phNum, fee, email, 500050);
+    }
+
+    public ConstructorDemoTwo(int id, String name, String course, long phNum, double fee, int pincode) {
+        this(id, name, course, phNum, fee, "", pincode);
+    }
+
+    public ConstructorDemoTwo(int id, String name, String course, long phNum, double fee, String email, int pincode) {
         this.id = id;
         this.name = name;
         this.course = course;
         this.phNum = phNum;
         this.fee = fee;
+        this.email = email;
+        this.pincode = pincode;
     }
 
 
     public void printInfo(){
-        System.out.printf("id = %d\nname = %s\ncourse = %s\nphone num = %d\nfee = %f\n", id, name, course, phNum, fee);
+        System.out.printf("id = %d\nname = %s\ncourse = %s\nphone num = %d\nfee = %f\nemail = %s\npincode = %d\n", id, name, course, phNum, fee, email, pincode);
     }
 
     public static void main(String[] args) {
@@ -44,6 +56,15 @@ public class ConstructorDemoTwo {
 
         ConstructorDemoTwo obj2 = new ConstructorDemoTwo(102, "John", "Python", 6751122334L, 8000.00);
         obj2.printInfo();
+
+        ConstructorDemoTwo obj3 = new ConstructorDemoTwo(103, "Jefry", "Python", 9988812345L, 6000.00, "jefry@gmail.com");
+        obj3.printInfo();
+
+        ConstructorDemoTwo obj4 = new ConstructorDemoTwo(104, "Dan", "Django", 8833312399L, 16000.00, 500085);
+        obj4.printInfo();
+
+        ConstructorDemoTwo obj5 = new ConstructorDemoTwo(105, "Tom", "Flask", 8976611223L, 16000.00, "tom@gmail.com", 500010);
+        obj5.printInfo();
     }
 
 
